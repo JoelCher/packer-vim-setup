278ab960e7fc1555f6ecb175c2baab64596374e5
JoelCher <joelcher@hilma.tech>
2025-06-22 11:55:09 +0300

    init

--- init.lua
+++ init.lua
@@ -18,6 +18,22 @@ lspconfig.clangd.setup({
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("html", {
+	capabilities = capabilities,
+})
+
+vim.lsp.config("cssls", {
+	capabilities = capabilities,
+})
+
+vim.lsp.enable("html")
+vim.lsp.enable("htmx")
+vim.lsp.enable("cssls")
+
 -- require("cssls").setup({})
 
 lspconfig.arduino_language_server.setup({
